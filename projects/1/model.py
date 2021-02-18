9f67adadf2a622deaddd2f1b8977f8d8b408931e
Tingol-2 <78476921+Tingol-2@users.noreply.github.com>
2021-02-18 20:28:25 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -11,10 +11,25 @@ categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 
 fields = ["id", "label"] + numeric_features + categorical_features
 
+categorical_to_transform = ['cf6', 'cf9', 'cf13', 'cf16', 'cf17', 'cf19', 'cf25', 'cf26']   #cf8,cf18 
+
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median'))
-    
+    ('scaler', StandardScaler())
+])
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
     ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_to_transform)
+    ]
+)
+
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('linearregression', LogisticRegression())
 ])
