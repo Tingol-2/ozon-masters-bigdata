2b2c85d8bd3decc8de3f4d8fa45e73e41d596c81
Tingol-2 <78476921+Tingol-2@users.noreply.github.com>
2022-03-01 16:28:36 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -2,23 +2,22 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
-
 
 numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 
 fields = ["id", "label"] + numeric_features + categorical_features
 
-#categorical_to_transform = ['cf6', 'cf9', 'cf13', 'cf16', 'cf17', 'cf19', 'cf25', 'cf26']   #cf8,cf18 
-
-categorical_to_transform = ['cf6', 'cf9']   #cf8,cf18 
+used_categorical_features = \
+['cf6', 'cf9', 'cf13', 'cf16', 'cf17', 'cf19', 'cf25', 'cf26', 'day_number']
 
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
     ('scaler', StandardScaler())
 ])
+
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
     ('onehot', OneHotEncoder(handle_unknown='ignore'))
@@ -27,11 +26,11 @@ categorical_transformer = Pipeline(steps=[
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', numeric_transformer, numeric_features),
-        ('cat', categorical_transformer, categorical_to_transform)
+        ('cat', categorical_transformer, used_categorical_features)
     ]
 )
 
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
-    ('linearregression', LogisticRegression())
+    ('logisticregression', LogisticRegression(random_state=42, n_jobs=-1, max_iter=1000))
 ])
