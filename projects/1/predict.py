3349271dabed8df2b06bbf050140bc004f34e48c
Tingol-2 <78476921+Tingol-2@users.noreply.github.com>
2022-03-04 13:18:52 +0300

    Update predict.py

--- projects/1/predict.py
+++ projects/1/predict.py
@@ -29,5 +29,5 @@ read_opts=dict(
 for df in pd.read_csv(sys.stdin, **read_opts):
     pred = model.predict_proba(df.iloc[:, 1:])
     out = zip(df.id, pred[:, 1])
-    print("\n".join(["{0},{1}".format(*i) for i in out]))
+    print("\n".join(["{0}\t{1}".format(*i) for i in out]))
 logging.info("done!")
