c2d43929884cf57bc982695cddae2024d2048534
Tingol-2 <78476921+Tingol-2@users.noreply.github.com>
2022-03-12 19:12:56 +0300

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -20,15 +20,29 @@ logging.info("ARGS {}".format(sys.argv[1:]))
 #load the model
 model = load("2.joblib")
 
+#read and infere
+#read_opts=dict(
+#        sep='\t', names=["id"] + numeric_features + categorical_features, index_col=False, header=None,
+#        iterator=True, chunksize=100
+#)
+
 read_opts = dict(
     sep='\t', names=["id"] + numeric_features + categorical_features,
     index_col=False, header=None,
-    iterator=True, chunksize=200,
+    iterator=True, chunksize=100,
     na_values='\\N'
 )
 
 
 logging.info("Predict data ...")
+logging.info(numeric_features    )
+logging.info(categorical_features)
+
+for df in pd.read_csv(sys.stdin, **read_opts):
+    pred = model.predict_proba(df.iloc[:, 1:])
+    out = zip(df.id, pred[:, 1])
+    print("\n".join(["{0}\t{1}".format(*i) for i in out]))
+logging.info("... completed")o("Predict data ...")
 logging.info(numeric_features    )
 logging.info(categorical_features)
 
