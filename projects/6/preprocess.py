1c64571038bbfee956a4c967e8b6000025fd0c0f
Tingol-2 <78476921+Tingol-2@users.noreply.github.com>
2022-05-07 22:43:03 +0300

    Update preprocess.py

--- projects/6/preprocess.py
+++ projects/6/preprocess.py
@@ -57,7 +57,7 @@ def main():
     x = hasher.transform(x)
     x = vas.transform(x)
 
-    x.write().overwrite().save(path_out)
+    x.write.overwrite.json(path_out)
 
 if __name__ == "__main__":
     main()
