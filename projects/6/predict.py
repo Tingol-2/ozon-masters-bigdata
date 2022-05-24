9322e0742b2a72cc9ee3f89ecf8bc28d615796d2
Tingol-2 <78476921+Tingol-2@users.noreply.github.com>
2022-05-24 23:02:31 +0300

    Update predict.py

--- projects/6/predict.py
+++ projects/6/predict.py
@@ -1,4 +1,5 @@
 #!/opt/conda/envs/dsenv/bin/python
+from joblib import load
 import os, sys
 import pandas as pd
 from pyspark.sql import SparkSession
@@ -6,7 +7,9 @@ from pyspark.sql.types import *
 from pyspark import SparkConf
 import pyspark.sql.functions as F
 from pyspark.ml.functions import vector_to_array
-from pyspark.sql.types import StructType, StructField, DoubleType, StringType, BooleanType, TimestampType, FloatType
+from pyspark.sql.types import StructType, StructField, DoubleType, StringType, BooleanType, TimestampType
+from pyspark.ml.linalg import VectorUDT
+
 
 SPARK_HOME = "/usr/hdp/current/spark2-client"
 PYSPARK_PYTHON = "/opt/conda/envs/dsenv/bin/python"
@@ -36,7 +39,26 @@ def predict(series):
     predictions = est_broadcast.value.predict(series.tolist())
     return pd.Series(predictions)
 
-dataset = spark.read.json(path_in)
+schema = StructType([
+    StructField("label", DoubleType()),
+    StructField("vote", StringType()),
+    StructField("verified", BooleanType()),
+    StructField("reviewTime", StringType()),
+    StructField("reviewerID", StringType()),
+    StructField("asin", StringType()),
+    StructField("id", StringType()),
+    StructField("reviewerName", StringType()),
+    StructField("reviewText", StringType()),
+    StructField("summary", StringType()),
+    StructField("unixReviewTime", TimestampType()),
+    StructField("words", VectorUDT()),
+    StructField("words_filtered", VectorUDT()),
+    StructField("word_vec", VectorUDT()),
+    StructField("features", VectorUDT())
+])
+
+
+dataset = spark.read.json(path_in, schema=schema)
 
 predicts = dataset.withColumn('predictions', predict(vector_to_array('features')))
 
